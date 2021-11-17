ce572e22b52b98cb6c429e02dfcc4f5ef787fd6d
ValeedAnjum <allinone644@gmail.com>
2021-11-17 19:01:14 +0500

    Explored a little about style tag sx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import MyCard from './components/card/Card'
+import AppBar from './components/appbar/Appbar'
+import { Fragment } from 'react';
 
 function App() {
   return (
-   <MyCard />
+    <Fragment>
+      <AppBar />
+
+    </Fragment>
   );
 }
 
